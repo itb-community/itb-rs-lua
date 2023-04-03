c4f42ea6d9fef75adfbe20c596ed29cbbe051070
kartoFlane <tbachminski@gmail.com>
2023-04-03 02:04:34 +0200

    Consume the package variable to release resources

--- src/ftldat/lua_exports.rs
+++ src/ftldat/lua_exports.rs
@@ -27,64 +27,82 @@ fn read(_: &Lua, (path, ): (String, )) -> LuaResult<LuaPackageWrapper> {
 }
 //endregion
 
-struct LuaPackageWrapper {
-    package: Package,
-}
+struct LuaPackageWrapper(Option<Package>);
 
 impl LuaPackageWrapper {
     fn new() -> LuaPackageWrapper {
-        LuaPackageWrapper {
-            package: Package::new()
-        }
+        LuaPackageWrapper(Some(Package::new()))
+    }
+
+    fn package(&mut self) -> Package {
+        self.0.take()
+            .expect("This instance has already been written out, and is no longer open.")
+    }
+
+    fn package_ref(&self) -> &Package {
+        & self.0.as_ref()
+            .expect("This instance has already been written out, and is no longer open.")
+    }
+
+    fn package_mut(&mut self) -> &mut Package {
+        self.0.as_mut()
+            .expect("This instance has already been written out, and is no longer open.")
     }
 
     fn read_from_path<P: AsRef<Path>>(path: P) -> Result<LuaPackageWrapper, PackageReadError> {
-        ftldat::dat::read_from_path(path)
+        ftldat::dat::read_package_from_path(path)
             .map(|package| {
-                LuaPackageWrapper { package }
+                LuaPackageWrapper(Some(package))
             })
     }
 }
 
 impl LuaUserData for LuaPackageWrapper {
     fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
-        methods.add_method("to_file", |_, this, (path, ): (String, )| {
-            ftldat::dat::write_package_to_path(&this.package, &path)
+        methods.add_method_mut("to_file", |_, this, (path, ): (String, )| {
+            ftldat::dat::write_package_into_path(this.package(), &path)
                 .map_err(external_lua_error)
         });
 
+        methods.add_method_mut("destroy", |_, this, ()| {
+            if this.0.is_some() {
+                this.0.take();
+            }
+            Ok(())
+        });
+
         methods.add_method_mut("add_entry_from_string", |_, this, (path, content): (String, String)| {
-            this.package.add_entry(PackageEntry::from_string(path, content))
+            this.package_mut().add_entry(PackageEntry::from_string(path, content))
                 .map_err(external_lua_error)
         });
 
         methods.add_method_mut("add_entry_from_byte_array", |_, this, (path, content): (String, Vec<u8>)| {
-            this.package.add_entry(PackageEntry::from_byte_array(path, content))
+            this.package_mut().add_entry(PackageEntry::from_byte_array(path, content))
                 .map_err(external_lua_error)
         });
 
         methods.add_method_mut("add_entry_from_file", |_, this, (path, source_path): (String, String)| {
-            this.package.add_entry(PackageEntry::from_file(path, source_path))
+            this.package_mut().add_entry(PackageEntry::from_file(path, source_path))
                 .map_err(external_lua_error)
         });
 
         methods.add_method_mut("put_entry_from_string", |_, this, (path, content): (String, String)| {
-            this.package.put_entry(PackageEntry::from_string(path, content));
+            this.package_mut().put_entry(PackageEntry::from_string(path, content));
             Ok(())
         });
 
         methods.add_method_mut("put_entry_from_byte_array", |_, this, (path, content): (String, Vec<u8>)| {
-            this.package.put_entry(PackageEntry::from_byte_array(path, content));
+            this.package_mut().put_entry(PackageEntry::from_byte_array(path, content));
             Ok(())
         });
 
         methods.add_method_mut("put_entry_from_file", |_, this, (path, source_path): (String, String)| {
-            this.package.put_entry(PackageEntry::from_file(path, source_path));
+            this.package_mut().put_entry(PackageEntry::from_file(path, source_path));
             Ok(())
         });
 
         methods.add_method("read_content_as_string", |_, this, (path, ): (String, )| {
-            let maybe_bytes = this.package.content_by_path(path);
+            let maybe_bytes = this.package_ref().content_by_path(path);
             match maybe_bytes {
                 None => Ok(None),
                 Some(bytes) => {
@@ -96,36 +114,36 @@ impl LuaUserData for LuaPackageWrapper {
         });
 
         methods.add_method("read_content_as_byte_array", |_, this, (path, ): (String, )| {
-            let maybe_bytes = this.package.content_by_path(path);
+            let maybe_bytes = this.package_ref().content_by_path(path);
             Ok(maybe_bytes)
         });
 
         methods.add_method_mut("remove", |_, this, (path, ): (String, )| {
-            Ok(this.package.remove_entry(path))
+            Ok(this.package_mut().remove_entry(path))
         });
 
         methods.add_method("exists", |_, this, (path, ): (String, )| {
-            Ok(this.package.entry_exists(&path))
+            Ok(this.package_ref().entry_exists(&path))
         });
 
         methods.add_method_mut("clear", |_, this, ()| {
-            Ok(this.package.clear())
+            Ok(this.package_mut().clear())
         });
 
         methods.add_method("inner_paths", |_lua, this, ()| {
-            Ok(this.package.inner_paths())
+            Ok(this.package_ref().inner_paths())
         });
 
         methods.add_method("len", |_, this, ()| {
-            Ok(this.package.entry_count())
+            Ok(this.package_ref().entry_count())
         });
 
         methods.add_method("entry_count", |_, this, ()| {
-            Ok(this.package.entry_count())
+            Ok(this.package_ref().entry_count())
         });
 
         methods.add_method("extract", |_, this, (path, ): (String, )| {
-            this.package.extract(path)
+            this.package_ref().extract(path)
                 .map_err(external_lua_error)
         });
     }
