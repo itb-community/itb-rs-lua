80109e25a246272eab573ffd16b076bd944e9ba3
kartoFlane <tbachminski@gmail.com>
2023-04-16 02:37:27 +0200

    Fix Directory.relative_path returning double trailing slash
    Also remove `normalize` invocation from both File and Directory functions,
    since the path is already normalized by the inner `relativize` call

--- src/io/directory.rs
+++ src/io/directory.rs
@@ -21,7 +21,6 @@ impl Directory {
 
     pub fn relative_path(&self) -> std::io::Result<String> {
         let normalized_path_relative_to_root = self.root()?.relativize(&self.path)
-            .map(|relative_path| normalize(relative_path) + "/")
             .unwrap_or_else(|| "".to_string());
 
         Ok(normalized_path_relative_to_root)
